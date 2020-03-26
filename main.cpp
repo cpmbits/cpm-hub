340850b1882a5a9389444cc25fc67344f6d98f16
Jordi Sánchez <jordi@pop-os.localdomain>
2020-03-26 21:11:53 +0100

    add missing plugin to project descriptor

--- main.cpp
+++ main.cpp
@@ -15,7 +15,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-#include <iostream>
 #include <string>
 #include <boost/program_options.hpp>
 #include <inih/INIReader.h>
