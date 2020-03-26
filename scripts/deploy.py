e014811912ed6daeaaed45250ec52fcb8dbb7fc0
Jordi Sánchez <jordi@pop-os.localdomain>
2020-03-26 19:21:44 +0100

    implemented deploy script

---
+++ scripts/deploy.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3.7
+import requests
+import base64
+import argparse
+import zipfile
+
+parser = argparse.ArgumentParser(prog='deploy cpm-hub', description='CPM Hub deployer', add_help=False)
+parser.add_argument('-k', '--api-key', required=True, dest="api_key")
+parser.add_argument('program_binary')
+parser.add_argument('server_url')
+args = parser.parse_args()
+
+zip_name = f'{args.program_binary}.zip'
+
+zip_file = zipfile.ZipFile(zip_name, 'w', zipfile.ZIP_DEFLATED)
+zip_file.write(args.program_binary)
+zip_file.close()
+
+with open(zip_name, 'rb') as stream:
+    binary = stream.read()
+    payload = base64.b64encode(binary).decode('utf-8')
+
+headers = {'API_KEY': args.api_key}
+requests.post(args.server_url, data=payload, headers=headers, verify=False)
