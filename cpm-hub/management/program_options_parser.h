1cba80a9b90d11702e580544292dc0666e6e8053
Jordi Sánchez <jorsanpe@gmail.com>
2020-05-07 20:19:59 +0200

    move program options parsing from main to management package (#54)
    Co-authored-by: Jordi Sánchez <jordi.sanchez@casa-systems.com>

--- cpm-hub/management/program_options_parser.h
+++ cpm-hub/management/program_options_parser.h
@@ -1,49 +1,25 @@
 /*
  * Copyright (C) 2020  Jordi Sánchez
  * This file is part of CPM Hub
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+#pragma once
+
 #include <vector>
 #include <string>
 
-#include <management/program_options_parser.h>
-#include <management/cpm_hub_starter.h>
+#include <management/ProgramOptions.h>
 
-using namespace std;
-
-
-static vector<string> asVector(int argc, char **argv)
-{
-    vector<string> command_line;
-
-    for (int i=0; i<argc; i++) {
-        command_line.emplace_back(string(argv[i]));
-    }
-
-    return command_line;
-}
-
-
-int main(int argc, char *argv[])
-{
-    ProgramOptions program_options;
-    vector<string> command_line = asVector(argc, argv);
-
-    program_options = parseProgramOptions(argc, argv);
-
-    startCpmHub(program_options, command_line);
-
-    return 0;
-}
+ProgramOptions parseProgramOptions(int argc, char *argv[]);
